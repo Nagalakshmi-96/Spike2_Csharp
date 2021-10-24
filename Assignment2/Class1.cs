31fb18657b9d1468565ebdbc08d432f6e5a2a558
Nagalakshmi-96 <93072593+Nagalakshmi-96@users.noreply.github.com>
2021-10-24 21:16:26 +0530

    assignment 2 changes

---
+++ Assignment2/Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class OverlappingDays
+    {
+        public string FromID { get; set; }
+        public string ToID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+    public class Dates
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+    public class Class1
+    {
+        static void Main()
+        {
+
+            List<OverlappingDays> data = new List<OverlappingDays>();
+
+            data.Add(new OverlappingDays { FromID = "S1", ToID = "S2", StartDate = new DateTime(2016, 1, 1), EndDate = new DateTime(2016, 1, 15) });
+            
+            //Test Cases
+            //data.Add(new OverlappingDays { FromID = "S2", ToID = "S3", StartDate = new DateTime(2016, 3, 16), EndDate = new DateTime(2016, 3, 17) });
+
+            data.Add(new OverlappingDays { FromID = "S2", ToID = "S3", StartDate = new DateTime(2016, 1, 25), EndDate = new DateTime(2016, 2, 25) });
+
+            data.Add(new OverlappingDays { FromID = "S2", ToID = "S3", StartDate = new DateTime(2016, 2, 1), EndDate = new DateTime(2016, 3, 14) });
+
+            data.Add(new OverlappingDays { FromID = "S1", ToID = "S2", StartDate = new DateTime(2016, 1, 21), EndDate = new DateTime(2016, 1, 25) });
+            
+            data.Add(new OverlappingDays { FromID = "S1", ToID = "S2", StartDate = new DateTime(2016, 1, 5), EndDate = new DateTime(2016, 1, 20) });
+
+            Console.WriteLine("\n*************Original Table*************\n");
+            foreach (var o in data)
+            {
+                Console.WriteLine("{0}       {1}       {2}       {3}", o.FromID, o.ToID, o.StartDate.ToShortDateString(), o.EndDate.ToShortDateString());
+            }
+            
+            Dictionary<Tuple<string, string>, List<Dates>> dict = new Dictionary<Tuple<string, string>, List<Dates>>();
+
+
+            Dictionary<Tuple<string, string>, List<Dates>> dict1 = new Dictionary<Tuple<string, string>, List<Dates>>();
+
+            foreach (var o in data)
+            {
+                var tupl = new Tuple<string, string>(o.FromID, o.ToID);
+                var list = new Dates { StartDate = o.StartDate, EndDate = o.EndDate };
+                if (dict.ContainsKey(tupl))
+                {
+                    dict[tupl].Add(list);
+                }
+                else
+                {
+                    List<Dates> list1 = new List<Dates>();
+                    list1.Add(list);
+                    dict.Add(tupl, list1);
+                }
+            }
+
+            
+            
+            foreach(var a in dict)
+            {    
+                dict[a.Key].Sort((x, y) => x.StartDate.CompareTo(y.StartDate));
+                for(int i=0;i< dict[a.Key].Count-1;i++)
+                {
+                    if(dict[a.Key][i].EndDate > dict[a.Key][i+1].StartDate || (dict[a.Key][i].EndDate.AddDays(1)==dict[a.Key][i+1].StartDate))
+                    {
+                        if(dict[a.Key][i].EndDate <= dict[a.Key][i+1].EndDate)
+                        {
+                            dict[a.Key][i].EndDate = dict[a.Key][i + 1].EndDate;
+                            dict[a.Key].RemoveAt(i + 1);
+                            i--;
+                        }
+                        
+                    }
+                }
+                
+            }
+
+            foreach (var a in dict1)
+            {
+                
+                foreach(var o in dict1[a.Key])
+                {
+                    Console.WriteLine("{0}   {1}    {2}", a.Key, o.StartDate.ToShortDateString(), o.EndDate.ToShortDateString());
+                }
+            }
+
+                Console.Read();
+        }
+    }
+}
---
+++ Assignment2/Class2.cs
@@ -0,0 +1,95 @@
+﻿//using System;
+//using System.Linq;
+//using System.Data;
+//using System.Collections.Generic;
+
+//public class Program
+//{
+//    public class Dates
+//    {
+//        public DateTime ActiveDate;
+//        public DateTime ExpireDate;
+//        public string Name;
+//    }
+
+//    public static void Main()
+//    {
+//        original code.
+//        List<Dates> datesList = new List<Dates>();
+//        datesList.Add(new Dates { Name = "A", ActiveDate = new DateTime(2017, 8, 20), ExpireDate = new DateTime(2017, 8, 23) }); //Aug 20 - Aug 23
+//        datesList.Add(new Dates { Name = "B", ActiveDate = new DateTime(2017, 8, 20), ExpireDate = new DateTime(2017, 8, 22) }); //Aug 20 - Aug 22
+//        datesList.Add(new Dates { Name = "C", ActiveDate = new DateTime(2017, 8, 22, 23, 0, 0), ExpireDate = new DateTime(2017, 8, 26) }); //Aug 22 11pm - Aug 26
+//        datesList.Add(new Dates { Name = "D", ActiveDate = new DateTime(2017, 8, 20), ExpireDate = new DateTime(2017, 8, 23) }); //Aug 20 - Aug23
+
+
+//        extrapolate the actual arrays of days that belong to each "Dates" object
+//       var compareRanges = datesList.Select(d => GetDateRange(d)).ToList();
+
+//        foreach (var o in compareRanges)
+//        {
+//            Console.WriteLine(o);
+//        }
+
+//        do a bit of LINQ here...
+//        var result = CompareDates(compareRanges, 3);
+
+//        output the results.
+//       Console.WriteLine("Overlaps");
+//        foreach (var dt in result)
+//        {
+//            Console.WriteLine(dt.ToString("M/d/yyyy"));
+//        }
+
+//        var maxOverlap = GetLongestOverlap(compareRanges);
+
+//        Console.WriteLine();
+//        Console.WriteLine("Most overlaps: " + maxOverlap.Item2);
+//        foreach (var dt in maxOverlap.Item1)
+//        {
+//            Console.WriteLine(dt.ToString("M/d/yyyy"));
+//        }
+
+//        Console.Read();
+//    }
+
+
+//    private static DateTime[] GetDateRange(Dates dt)
+//    {
+
+//        var start = dt.ActiveDate.Date;
+//        var end = dt.ExpireDate.Date;
+
+
+//        return Enumerable.Range(0, Convert.ToInt32((end.Date - start.Date).TotalDays) + 1)
+//            .Select(e => start.Date.AddDays(e)).ToArray();
+//    }
+
+//    set the threshold to filter the overlapped dates...
+//    private static List<DateTime> CompareDates(List<DateTime[]> compareRanges, int overlapThreshold = 1)
+//    {
+//        merge all the days into a grouped listing
+//        var grouped = compareRanges.SelectMany(r => r).GroupBy(d => d.Date);
+
+//        filter the grouped list based on the overlap threshold, and pick out the appropriate dates
+//       var thresholdMatch = grouped.Where(g => g.Count() >= overlapThreshold)
+//           .Select(g => g.Key)
+//           .OrderBy(d => d)
+//           .ToList();
+
+//        return thresholdMatch;
+//    }
+
+
+//    which date in the ranges list has the most overlaps
+//    private static Tuple<List<DateTime>, int> GetLongestOverlap(List<DateTime[]> compareRanges)
+//    {
+//        var grouped = compareRanges.SelectMany(r => r).GroupBy(d => d.Date);
+//        var max = grouped.Max(g => g.Count());
+//        var maxMatch = grouped.Where(g => g.Count() == max)
+//            .Select(g => g.Key)
+//            .OrderBy(d => d)
+//            .ToList();
+
+//        return new Tuple<List<DateTime>, int>(maxMatch, max);
+//    }
+//}
